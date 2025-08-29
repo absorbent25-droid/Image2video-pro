1b12c04f71edff102c20c6a9c27d0854d377bd8b
absorbent25-droid <absorbent25@gmail.com>
2025-08-29 17:10:34 +0500

    Update Input.tsx

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,12 +1,12 @@
-import { InputHTMLAttributes } from "react";
+import React from "react";
 
-type InputProps = InputHTMLAttributes<HTMLInputElement>;
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-export default function Input(props: InputProps) {
+export function Input(props: InputProps) {
   return (
     <input
-      className="w-full px-4 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
       {...props}
+      className="px-3 py-2 border rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
     />
   );
 }
