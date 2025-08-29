60593829d5f402cbd8d0ae3bbca01932f6f983f0
absorbent25-droid <absorbent25@gmail.com>
2025-08-29 23:28:36 +0500

    Update Card.tsx

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,1 +1,18 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
+export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function Card({ className, children, ...props }: CardProps) {
+  return (
+    <div
+      className={cn(
+        "rounded-xl border bg-white shadow p-4",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
