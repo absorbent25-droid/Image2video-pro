9308c8db49e9178d8dc7072552df7a83ea98fe02
absorbent25-droid <absorbent25@gmail.com>
2025-08-29 23:44:54 +0500

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,6 @@
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-/**
- * Функция объединяет несколько классов tailwind без конфликтов.
- * Пример:
- * cn("px-2 py-1", condition && "bg-red-500")
- */
 export function cn(...inputs: any[]) {
   return twMerge(clsx(inputs))
 }
